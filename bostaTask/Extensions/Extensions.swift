04071ca52b22adeab6549a5a86a51e3dc7c6fc48
Abdelrahman <abdelrahman.tharwat95@gmail.com>
2023-09-07 02:01:29 +0300

    Add photos view controller

---
+++ bostaTask/Extensions/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  bostaTask
+//
+//  Created by Abdelrahman on 07/09/2023.
+//
+
+import Foundation
---
+++ bostaTask/Views/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  bostaTask
+//
+//  Created by Abdelrahman on 07/09/2023.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}

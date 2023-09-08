2440bb6a9d31afb416f5e505e961d7df69e6fefb
Abdelrahman <abdelrahman.tharwat95@gmail.com>
2023-09-08 17:50:29 +0300

    Add image view and search functionality

---
+++ bostaTask/Managers/AlertManager.swift
@@ -0,0 +1,8 @@
+//
+//  AlertManager.swift
+//  bostaTask
+//
+//  Created by Abdelrahman on 08/09/2023.
+//
+
+import Foundation
---
+++ bostaTask/Protocols/ViewModelsDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelsDelegate.swift
+//  bostaTask
+//
+//  Created by Abdelrahman on 08/09/2023.
+//
+
+import Foundation
